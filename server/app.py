afac2ee4194edf9e69095da7ccb17d1b69887465
Giselle <gisellec60@gmail.com>
2023-12-27 14:34:17 -0500

    making sure server and client both work

---
+++ server/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import smtplib
+import json
+import os
+from flask import request, session, make_response, render_template, redirect, current_app
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+from config import app, db, api
+from datetime import date, datetime
+
+@app.route('/')
+@app.route('/<int:id>')
+def index(id=0):
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(port=5555,debug=True)
