9d9f19a7e085e5e7505a2b3a8dd6368384cba7db
Giselle <gisellec60@gmail.com>
2024-01-01 15:03:16 -0500

    added heading page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import "./homestyles.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container'
+import Row from  'react-bootstrap/Row'
+import Col from  'react-bootstrap/Col'
+import { useWindowSize } from "@uidotdev/usehooks";
+
 
 function Home() {
+  const size = useWindowSize
+  console.log("this is size", size.width)
   return (
     <div>
-      
+      <Container fluid className='noWrap'></Container>
+      <Row>
+                <Col className="col" >
+                    <h1 className='productions-heading'>Defect System </h1> 
+                </Col>
+         </Row>
     </div>
   )
 }
